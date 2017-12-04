86140350d4aa5c319fb2df1efc7193232afcd84b
Matthew McConaghie <matthewmcconaghie@MacBook-Pro.Home>
2017-12-03 20:17:15 -0700

    configure connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,15 @@
 var mongoose = require("mongoose");
 
 // Local Database Configuration with Mongoose
-// mongoose.connect("mongodb://localhost/goodnews", function(error) {
-//     if (error) throw error;
-//     console.log("Database connected");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/goodnews";
+
+mongoose.connect(MONGODB_URI, function(error) {
+    if (error) throw error;
+    console.log("Database connected");
+});
+
+// mLab database
+// mongoose.connect("mongodb://heroku_tjfptdzd:496k70poqndda9fd5uub3qe2g4@ds125146.mlab.com:25146/heroku_tjfptdzd", function(err) {
+//     if (err) throw err;
+//     console.log('database connected');
 // });
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/goodnews",
-    function(error) {
-        if (error) throw error;
-        console.log("Database connected");;
-
-        // mLab database
-        mongoose.connect(MONGODB_URI, function(err) {
-            if (err) throw err;
-            console.log('database connected');
-        });
-
-        // var "mongodb://heroku_tjfptdzd:496k70poqndda9fd5uub3qe2g4@ds125146.mlab.com:25146/heroku_tjfptdzd"
