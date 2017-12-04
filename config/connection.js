3b68617cdb18fe47341689f5a157fa1604763b3c
Matthew McConaghie <matthewmcconaghie@MacBook-Pro.Home>
2017-12-03 20:05:54 -0700

    database change

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,15 @@ var mongoose = require("mongoose");
 //     console.log("Database connected");
 // });
 
-// mLab database
-mongoose.connect("mongodb://heroku_tjfptdzd:496k70poqndda9fd5uub3qe2g4@ds125146.mlab.com:25146/heroku_tjfptdzd", function(err) {
-    if (err) throw err;
-    console.log('database connected');
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/goodnews",
+    function(error) {
+        if (error) throw error;
+        console.log("Database connected");;
+
+        // mLab database
+        mongoose.connect(MONGODB_URI, function(err) {
+            if (err) throw err;
+            console.log('database connected');
+        });
+
+        // var "mongodb://heroku_tjfptdzd:496k70poqndda9fd5uub3qe2g4@ds125146.mlab.com:25146/heroku_tjfptdzd"
